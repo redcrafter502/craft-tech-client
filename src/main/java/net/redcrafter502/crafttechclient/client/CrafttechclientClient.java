3689d647afcf21948199a0b58b97911a11ef417d
redcrafter502 <oskar.bahr.lg@gmail.com>
2022-02-23 19:06:55 +0100

    Initial Commit

---
+++ src/main/java/net/redcrafter502/crafttechclient/client/CrafttechclientClient.java
@@ -0,0 +1,13 @@
+package net.redcrafter502.crafttechclient.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class CrafttechclientClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
