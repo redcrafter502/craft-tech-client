119cb62231856f3fec4dbd0488d7a77fef8a096c
redcrafter502 <oskar.bahr.lg@gmail.com>
2022-08-10 12:24:14 +0200

    [#4] Added Zoom Key Binding

--- src/main/kotlin/net/redcrafter502/crafttechclient/client/CrafttechclientClient.kt
+++ src/main/kotlin/net/redcrafter502/crafttechclient/client/CrafttechclientClient.kt
@@ -2,11 +2,18 @@ package net.redcrafter502.crafttechclient.client
 
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
 import net.fabricmc.api.Environment
+import net.minecraft.client.util.InputUtil
+import net.minecraft.client.option.KeyBinding
 
 @Environment(EnvType.CLIENT)
 class CrafttechclientClient : ClientModInitializer {
-    override fun onInitializeClient() {
+    private lateinit var zoomKeyBinding: KeyBinding
 
+    override fun onInitializeClient() {
+        // Key Bindings
+        zoomKeyBinding = KeyBinding("key.crafttechclient.zoom", InputUtil.Type.KEYSYM, InputUtil.GLFW_KEY_C, "category.crafttechclient.zoom")
+        KeyBindingHelper.registerKeyBinding(zoomKeyBinding)
     }
 }
