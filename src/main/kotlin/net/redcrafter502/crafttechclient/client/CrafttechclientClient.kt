471024b99acf8970eebc322172c303376af06369
redcrafter502 <oskar.bahr.lg@gmail.com>
2022-08-10 20:01:07 +0200

    [#4] Added Zoom functionality

--- src/main/kotlin/net/redcrafter502/crafttechclient/client/CrafttechclientClient.kt
+++ src/main/kotlin/net/redcrafter502/crafttechclient/client/CrafttechclientClient.kt
@@ -4,16 +4,85 @@ import net.fabricmc.api.EnvType
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
 import net.fabricmc.api.Environment
+import net.minecraft.client.MinecraftClient
 import net.minecraft.client.util.InputUtil
 import net.minecraft.client.option.KeyBinding
 
 @Environment(EnvType.CLIENT)
 class CrafttechclientClient : ClientModInitializer {
-    private lateinit var zoomKeyBinding: KeyBinding
 
     override fun onInitializeClient() {
         // Key Bindings
         zoomKeyBinding = KeyBinding("key.crafttechclient.zoom", InputUtil.Type.KEYSYM, InputUtil.GLFW_KEY_C, "category.crafttechclient.zoom")
         KeyBindingHelper.registerKeyBinding(zoomKeyBinding)
+
+        // Initialize Zoom Variables
+        currentlyZoomed = false
+        originalSmoothCameraEnabled = false
+    }
+
+    companion object {
+        // Key Bindings
+        private lateinit var zoomKeyBinding: KeyBinding
+
+        private var currentlyZoomed: Boolean = false
+        private var originalSmoothCameraEnabled: Boolean = false
+        private val mc: MinecraftClient = MinecraftClient.getInstance()
+
+        val zoomLevel: Double = 0.23
+
+        fun isZooming(): Boolean {
+            return zoomKeyBinding.isPressed
+        }
+
+        fun manageSmoothCamera() {
+            if (zoomStarting()) {
+                zoomStarted()
+                enableSmoothCamera()
+            }
+    
+            if (zoomStopping()) {
+                zoomStopped()
+                resetSmoothCamera()
+            }
+        }
+
+        private fun isSmoothCamera(): Boolean {
+            return mc.options.smoothCameraEnabled
+        }
+
+        private fun enableSmoothCamera() {
+            mc.options.smoothCameraEnabled = true
+        }
+
+        private fun disableSmoothCamera() {
+            mc.options.smoothCameraEnabled = false
+        }
+
+        private fun zoomStarting(): Boolean {
+            return isZooming() && !currentlyZoomed
+        }
+
+        private fun zoomStopping(): Boolean {
+            return !isZooming() && currentlyZoomed
+        }
+
+        private fun zoomStarted() {
+            originalSmoothCameraEnabled = isSmoothCamera()
+            currentlyZoomed = true
+        }
+
+        private fun zoomStopped() {
+            currentlyZoomed = false
+        }
+
+        private fun resetSmoothCamera() {
+            if (originalSmoothCameraEnabled) {
+                enableSmoothCamera()
+            } else {
+                disableSmoothCamera()
+            }
+        }
+
     }
 }
---
+++ src/main/kotlin/net/redcrafter502/crafttechclient/mixin/CrafttechclientMixin.kt
@@ -0,0 +1,28 @@
+package net.redcrafter502.crafttechclient.mixin
+
+import net.redcrafter502.crafttechclient.client.CrafttechclientClient
+
+import org.spongepowered.asm.mixin.Mixin
+import org.spongepowered.asm.mixin.injection.At
+import org.spongepowered.asm.mixin.injection.Inject
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable
+
+import net.fabricmc.api.EnvType
+import net.fabricmc.api.Environment
+
+import net.minecraft.client.render.GameRenderer
+
+@Environment(EnvType.CLIENT)
+@Mixin(GameRenderer::class)
+class CrafttechclientMixin {
+
+    @Inject(method = ["getFov(Lnet/minecraft/client/render/Camera;FZ)D"], at = [At("RETURN")], cancellable = true)
+    fun getZoomLevel(callbackInfo: CallbackInfoReturnable<Double>) {
+        if (CrafttechclientClient.isZooming()) {
+            val fov: Double = callbackInfo.returnValue
+            callbackInfo.returnValue = fov * CrafttechclientClient.zoomLevel
+        }
+
+        CrafttechclientClient.manageSmoothCamera()
+    }
+}
